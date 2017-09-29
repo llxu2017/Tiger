4041c4d248de06d76282b45ed49ce6ea31d03459
Linlin Xu <linlin.xu@live.com>
2017-09-29 00:13:16 -0400

    Frame WIP.

---
+++ 6_Frame/translate.c
@@ -0,0 +1,33 @@
+#include "translate.h"
+#include "frame.h"
+
+
+struct Tr_access_ { Tr_level level; F_access access; };
+
+Tr_accessList Tr_AccessList(Tr_access head, Tr_accessList tail)
+{
+	Tr_accessList list = checked_malloc(sizeof(*list));
+	list->head = head;
+	list->tail = tail;
+	return list;
+}
+
+Tr_level Tr_outermost(void)
+{
+
+}
+
+Tr_level Tr_newLevel(Tr_level parent, Temp_label name, U_boolList formals)
+{
+
+}
+
+Tr_accessList Tr_formals(Tr_level level)
+{
+
+}
+
+Tr_access Tr_allocLocal(Tr_level level, bool escape)
+{
+
+}
